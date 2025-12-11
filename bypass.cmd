fc988690b8df4cc7609dbe79713f865119dc4406
Chandler <chandlerkmorell@gmail.com>
2025-12-11 12:22:04 -0600

    Change Sysprep command to use /generalize option

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
 curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ChandlerMorell/bypassnro/refs/heads/main/unattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
+%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /generalize /unattend:C:\Windows\Panther\unattend.xml /reboot
